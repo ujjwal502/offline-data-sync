6d7eb94c087e1dab8d83dada1fb59015d50ea689
ujjwal502 <shyamkumartiwari405@gmail.com>
2024-11-13 17:37:43 +0530

    adds exports in index file

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { SyncManager } from "./SyncManager/sync-manager.js";
+export { ConflictResolver } from "./ConflictResolver/conflict-resolver.js";
+export { RetryManager } from "./RetryManager/retry-manager.js";
+export type { SyncConfig, SyncRecord, RetryConfig } from "./common.types.js";
